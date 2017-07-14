6ce19b44a7deaeda04704d268b2bb5e877befad4
sachdev18 <sdvr.18@gmail.com>
2017-07-14 18:22:41 +0530

    Friday 14-7-17

---
+++ src/app.js
@@ -0,0 +1,11 @@
+import React {Component} from 'react';
+
+export default class App extends Component {
+render(){
+  return(
+    <div className="appDiv">
+    <p>Hey there !!! I'm learning React!!!</p>
+    </div>
+  );
+}
+}
---
+++ webpack.config.js
@@ -0,0 +1,11 @@
+var path = require('path');
+
+module.exports = {
+  entry: './src/index.js';
+  output: {
+    filename: 'bundle.js',
+    path:
+      filename: 'bundle.js',
+      path: __dirname + '/build'
+  }
+};
